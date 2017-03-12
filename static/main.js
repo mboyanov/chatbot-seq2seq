f091864c6644cf12bf48165e86aa332290bba2d7
Martin Boyanov <mboyanov@gmail.com>
2017-03-12 21:43:41 +0200

    [improvement] UI improvements - scroll + bubbles

--- static/main.js
+++ static/main.js
@@ -6,24 +6,18 @@ nlp = window.nlp_compromise;
 var messages = [], //array that hold the record of each string in chat
   lastUserMessage = "", //keeps track of the most recent input string from the user
   botMessage = "", //var keeps track of what the chatbot is going to say
-  botName = 'QLBot', //name of the chatbot
-  talking = true; //when false the speach function doesn't work
+  botName = 'QLBot'; //name of the chatbot
 //
 
 //edit this function to change what the chatbot says
-function chatbotResponse() {
-  talking = true;
+function chatbotResponse(user_message) {
   botMessage = "I'm confused"; //the default message
-  $.getJSON( "http://localhost:5000", {"message": lastUserMessage}, function( data ) {
+  $.getJSON( "http://localhost:5000", {"message": user_message}, function( data ) {
     botMessage = data['message'];
-    messages.push("<b>" + botName + ":</b> " + botMessage);
-    // says the message using the text to speech function written below
-    Speech(botMessage);
-    //outputs the last few array elements of messages to html
-    for (var i = 1; i < 8; i++) {
-      if (messages[messages.length - i])
-        document.getElementById("chatlog" + i).innerHTML = messages[messages.length - i];
-    }
+    userMessage = "<p class='triangle-border left'><b>You:</b> " + user_message + "</p>"
+    $('#entries').append(userMessage);
+    $('#entries').append("<p class='triangle-border right'><b>" + botName + ":</b> " + botMessage + "</p>");
+    $("#entries").animate({ scrollTop: $('#entries').prop("scrollHeight")}, 200);
   });
 }
 
@@ -33,32 +27,16 @@ function newEntry() {
   //if the message from the user isn't empty then run
   if (document.getElementById("chatbox").value != "") {
     //pulls the value from the chatbox ands sets it to lastUserMessage
-    lastUserMessage = document.getElementById("chatbox").value;
+    lastUserMessage = $("#chatbox")[0].value;
     //sets the chat box to be clear
     document.getElementById("chatbox").value = "";
     //adds the value of the chatbox to the array messages
-    messages.push(lastUserMessage);
-    //Speech(lastUserMessage);  //says what the user typed outloud
     //sets the variable botMessage in response to lastUserMessage
-    chatbotResponse();
+    chatbotResponse(lastUserMessage);
   }
 }
 
-//text to Speech
-//https://developers.google.com/web/updates/2014/01/Web-apps-that-talk-Introduction-to-the-Speech-Synthesis-API
-function Speech(say) {
-  if ('speechSynthesis' in window && talking) {
-    var utterance = new SpeechSynthesisUtterance(say);
-    //msg.voice = voices[10]; // Note: some voices don't support altering params
-    //msg.voiceURI = 'native';
-    //utterance.volume = 1; // 0 to 1
-    //utterance.rate = 0.1; // 0.1 to 10
-    //utterance.pitch = 1; //0 to 2
-    //utterance.text = 'Hello World';
-    //utterance.lang = 'en-US';
-    //speechSynthesis.speak(utterance);
-  }
-}
+
 
 //runs the keypress() function when a key is pressed
 document.onkeypress = keyPress;
